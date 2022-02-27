12f9c49bad3953bb3e0fe31cd989b96969abe4ad
unknown <talhashaikh14@gmail.com>
2022-02-27 17:03:56 +0530

    Exclude Data source for sql

---
+++ crud/src/main/java/com/crud/operations/crud/CrudApplication.java
@@ -0,0 +1,14 @@
+package com.crud.operations.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
